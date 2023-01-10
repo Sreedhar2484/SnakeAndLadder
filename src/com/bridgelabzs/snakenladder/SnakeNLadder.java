dca7259aeb1901058e64e12a7a09781f15994c2f
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-10 08:43:59 +0530

    Added : Snake and Ladder Program

--- src/com/bridgelabzs/snakenladder/SnakeNLadder.java
+++ src/com/bridgelabzs/snakenladder/SnakeNLadder.java
@@ -6,9 +6,26 @@ public class SnakeNLadder {
     
     public static void main(String[] args) {
 		System.out.println("welcome to snake and ladder game ");
+        int position = 0;
 		Random rand = new Random();
         int DiceValue = 1+rand.nextInt(6);
         System.out.println("Dice Value is " + DiceValue);
+        int option = rand.nextInt(3);
+        System.out.println("Current Option : " +option);
+        if(option == 0){
+            System.out.println("No play");
+        }
+        else if(option==1){
+            System.out.println("Ladder");
+            position += DiceValue;
+            System.out.println("Current Position : "+position);
+        }
+        else{
+            System.out.println("Snake");
+            position += DiceValue;
+            System.out.println("Current Position : "+position);
+
+        }
         
     }
 }
